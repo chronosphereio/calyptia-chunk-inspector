918974782fea4d1b6dd018b997b7c7d0bb587200
lecaros <jose.lecaros@gmail.com>
2022-01-28 13:59:08 -0500

    add logging

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"flag"
-	"fmt"
+	"log"
 	"os"
 )
 
@@ -16,10 +16,16 @@ const MinRequiredFileLength = FileMetaBytesQuantity + 1
 
 func main() {
 
-	fileName := flag.String("file", "chunk.flb", "File to be processed")
+	fileName := flag.String("f", "chunk.flb", "File to be processed")
+	verbose := flag.Bool("v", false, "Activates verbose mode")
 	flag.Parse()
 
-	fmt.Printf("Filename %s\n", *fileName)
+	if *verbose {
+		log.SetOutput(os.Stdout)
+	} else {
+		log.SetOutput(os.Stderr)
+	}
+	log.Printf("Filename %s\n", *fileName)
 
 	f, err := os.Open(*fileName)
 
@@ -29,7 +35,7 @@ func main() {
 	fileSize := displayFileInfo(f)
 
 	if fileSize < MinRequiredFileLength {
-		fmt.Println("File seems corrupted. Aborting")
+		log.Println("File seems corrupted. Aborting")
 		os.Exit(1)
 	}
 
@@ -54,35 +60,35 @@ func main() {
 func readUserData(f *os.File, metadataLength uint16, fileSize int64) {
 	remainingBytes := fileSize - int64(FileMetaBytesQuantity+metadataLength)
 	bytesRead, content := readNBytesFromFile(f, remainingBytes)
-	fmt.Printf("%d bytes from User Content: %s\n", content, string(bytesRead[:content]))
+	log.Printf("%d bytes from User Content: %s\n", content, string(bytesRead[:content]))
 }
 
 func readMetadata(f *os.File, mLength uint16) {
 	bytesRead, content := readNBytesFromFile(f, int64(mLength))
-	fmt.Printf("%d bytes from Metadata: %s\n", content, string(bytesRead[:content]))
+	log.Printf("%d bytes from Metadata: %s\n", content, string(bytesRead[:content]))
 }
 
 func getMetadataLength(f *os.File) uint16 {
 	bytesRead, _ := readNBytesFromFile(f, MetadataLengthBytesQuantity)
 	data := binary.BigEndian.Uint16(bytesRead)
 
-	fmt.Printf("Metadata Length: %d\n", data)
+	log.Printf("Metadata Length: %d\n", data)
 	return data
 }
 
 func readPadding(f *os.File) {
 	bytesRead, content := readNBytesFromFile(f, CRCPaddingBytesQuantity)
-	fmt.Printf("%d bytes from Padding: %s\n", content, string(bytesRead[:content]))
+	log.Printf("%d bytes from Padding: %s\n", content, string(bytesRead[:content]))
 }
 
 func readCRC(f *os.File) {
 	bytesRead, content := readNBytesFromFile(f, CRCBytesQuantity)
-	fmt.Printf("%d bytes from CRC: %s\n", content, string(bytesRead[:content]))
+	log.Printf("%d bytes from CRC: %s\n", content, string(bytesRead[:content]))
 }
 
 func readHeader(f *os.File) ([]byte, int) {
 	bytesRead, content := readNBytesFromFile(f, HeaderBytesQuantity)
-	fmt.Printf("%d bytes from header: %s\n", content, string(bytesRead[:content]))
+	log.Printf("%d bytes from header: %s\n", content, string(bytesRead[:content]))
 	return bytesRead, content
 }
 
@@ -96,7 +102,7 @@ func readNBytesFromFile(file *os.File, bytesToRead int64) ([]byte, int) {
 func displayFileInfo(f *os.File) int64 {
 	fileInformation, err := f.Stat()
 	check(err)
-	fmt.Printf("File size: %d bytes\n", fileInformation.Size())
+	log.Printf("File size: %d bytes\n", fileInformation.Size())
 	return fileInformation.Size()
 }
 
