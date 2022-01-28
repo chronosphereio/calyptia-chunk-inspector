8b041a779fac490d3cb421993da110346be084f4
lecaros <jose.lecaros@gmail.com>
2022-01-28 14:49:02 -0500

    add verbose mode

--- main.go
+++ main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"encoding/binary"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
-const HeaderBytesQuantity = 2
-const CRCBytesQuantity = 4
-const CRCPaddingBytesQuantity = 16
-const MetadataLengthBytesQuantity = 2
-const FileMetaBytesQuantity = HeaderBytesQuantity + CRCBytesQuantity + CRCPaddingBytesQuantity + MetadataLengthBytesQuantity
-const MinRequiredFileLength = FileMetaBytesQuantity + 1
+const (
+	HeaderBytesQuantity         = 2
+	CRCBytesQuantity            = 4
+	CRCPaddingBytesQuantity     = 16
+	MetadataLengthBytesQuantity = 2
+	FileMetaBytesQuantity       = HeaderBytesQuantity + CRCBytesQuantity + CRCPaddingBytesQuantity + MetadataLengthBytesQuantity
+	MinRequiredFileLength       = FileMetaBytesQuantity + 1
+)
+
+var verbose *bool
 
 func main() {
 
-	fileName := flag.String("f", "chunk.flb", "File to be processed")
-	verbose := flag.Bool("v", false, "Activates verbose mode")
+	fileName := flag.String("file", "chunk.flb", "File to be processed")
+	verbose = flag.Bool("v", false, "Activates verbose mode")
+	//directory := flag.String("dir", ".", "Directory containing the file(s) to process")
 	flag.Parse()
 
 	if *verbose {
@@ -25,7 +31,7 @@ func main() {
 	} else {
 		log.SetOutput(os.Stderr)
 	}
-	log.Printf("Filename %s\n", *fileName)
+	fmt.Printf("Filename %s\n", *fileName)
 
 	f, err := os.Open(*fileName)
 
@@ -35,7 +41,7 @@ func main() {
 	fileSize := displayFileInfo(f)
 
 	if fileSize < MinRequiredFileLength {
-		log.Println("File seems corrupted. Aborting")
+		fmt.Println("File seems corrupted. Aborting")
 		os.Exit(1)
 	}
 
@@ -55,40 +61,53 @@ func main() {
 
 	err = f.Close()
 	check(err)
+
+	fmt.Println("OK")
 }
 
 func readUserData(f *os.File, metadataLength uint16, fileSize int64) {
 	remainingBytes := fileSize - int64(FileMetaBytesQuantity+metadataLength)
 	bytesRead, content := readNBytesFromFile(f, remainingBytes)
-	log.Printf("%d bytes from User Content: %s\n", content, string(bytesRead[:content]))
+	if *verbose {
+		fmt.Printf("%d bytes read from User Content: %s\n", content, string(bytesRead[:content]))
+	}
 }
 
 func readMetadata(f *os.File, mLength uint16) {
 	bytesRead, content := readNBytesFromFile(f, int64(mLength))
-	log.Printf("%d bytes from Metadata: %s\n", content, string(bytesRead[:content]))
+	if *verbose {
+		fmt.Printf("%d bytes read from Metadata: %s\n", content, string(bytesRead[:content]))
+	}
 }
 
 func getMetadataLength(f *os.File) uint16 {
 	bytesRead, _ := readNBytesFromFile(f, MetadataLengthBytesQuantity)
 	data := binary.BigEndian.Uint16(bytesRead)
-
-	log.Printf("Metadata Length: %d\n", data)
+	if *verbose {
+		fmt.Printf("Metadata Length: %d\n", data)
+	}
 	return data
 }
 
 func readPadding(f *os.File) {
 	bytesRead, content := readNBytesFromFile(f, CRCPaddingBytesQuantity)
-	log.Printf("%d bytes from Padding: %s\n", content, string(bytesRead[:content]))
+	if *verbose {
+		fmt.Printf("%d bytes read from Padding: %s\n", content, string(bytesRead[:content]))
+	}
 }
 
 func readCRC(f *os.File) {
 	bytesRead, content := readNBytesFromFile(f, CRCBytesQuantity)
-	log.Printf("%d bytes from CRC: %s\n", content, string(bytesRead[:content]))
+	if *verbose {
+		fmt.Printf("%d bytes from CRC: %s\n", content, string(bytesRead[:content]))
+	}
 }
 
 func readHeader(f *os.File) ([]byte, int) {
 	bytesRead, content := readNBytesFromFile(f, HeaderBytesQuantity)
-	log.Printf("%d bytes from header: %s\n", content, string(bytesRead[:content]))
+	if *verbose {
+		fmt.Printf("%d bytes from header: %s\n", content, string(bytesRead[:content]))
+	}
 	return bytesRead, content
 }
 
@@ -102,7 +121,9 @@ func readNBytesFromFile(file *os.File, bytesToRead int64) ([]byte, int) {
 func displayFileInfo(f *os.File) int64 {
 	fileInformation, err := f.Stat()
 	check(err)
-	log.Printf("File size: %d bytes\n", fileInformation.Size())
+	if *verbose {
+		fmt.Printf("File size: %d bytes\n", fileInformation.Size())
+	}
 	return fileInformation.Size()
 }
 
