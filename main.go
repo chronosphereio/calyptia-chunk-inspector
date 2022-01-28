c936610c82274c812532c1b235ea5607028a8871
lecaros <jose.lecaros@gmail.com>
2022-01-28 12:50:41 -0500

    first commit
    Signed-off-by: lecaros <jose.lecaros@gmail.com>

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+)
+
+const HeaderBytesQuantity = 2
+const CRCBytesQuantity = 4
+const CRCPaddingBytesQuantity = 16
+const MetadataLengthBytesQuantity = 2
+const FileMetaBytesQuantity = HeaderBytesQuantity + CRCBytesQuantity + CRCPaddingBytesQuantity + MetadataLengthBytesQuantity
+
+func main() {
+
+	f, err := os.Open("1-1642796694.873134717.flb")
+	//f, err := os.Open("test.txt")
+	check(err)
+	defer f.Close()
+
+	fileSize := displayFileInfo(f)
+
+	if fileSize < 25 {
+		fmt.Println("File seems corrupted. Aborting")
+		os.Exit(1)
+	}
+
+	readHeader(f)
+
+	readCRC(f)
+
+	readPadding(f)
+
+	mLength := getMetadataLength(f)
+
+	if mLength > 0 {
+		readMetadata(f, mLength)
+	}
+
+	readUserData(f, mLength, fileSize)
+
+	err = f.Close()
+	check(err)
+}
+
+func readUserData(f *os.File, metadataLength uint16, fileSize int64) {
+	remainingBytes := fileSize - int64(FileMetaBytesQuantity+metadataLength)
+	bytesRead, content := readNBytesFromFile(f, remainingBytes)
+	fmt.Printf("%d bytes from User Content: %s\n", content, string(bytesRead[:content]))
+}
+
+func readMetadata(f *os.File, mLength uint16) {
+	bytesRead, content := readNBytesFromFile(f, int64(mLength))
+	fmt.Printf("%d bytes from Metadata: %s\n", content, string(bytesRead[:content]))
+}
+
+func getMetadataLength(f *os.File) uint16 {
+	bytesRead, _ := readNBytesFromFile(f, MetadataLengthBytesQuantity)
+	data := binary.BigEndian.Uint16(bytesRead)
+
+	fmt.Printf("Metadata Length: %d\n", data)
+	return data
+}
+
+func readPadding(f *os.File) {
+	bytesRead, content := readNBytesFromFile(f, CRCPaddingBytesQuantity)
+	fmt.Printf("%d bytes from Padding: %s\n", content, string(bytesRead[:content]))
+}
+
+func readCRC(f *os.File) {
+	bytesRead, content := readNBytesFromFile(f, CRCBytesQuantity)
+	fmt.Printf("%d bytes from CRC: %s\n", content, string(bytesRead[:content]))
+}
+
+func readHeader(f *os.File) ([]byte, int) {
+	bytesRead, content := readNBytesFromFile(f, HeaderBytesQuantity)
+	fmt.Printf("%d bytes from header: %s\n", content, string(bytesRead[:content]))
+	return bytesRead, content
+}
+
+func readNBytesFromFile(file *os.File, bytesToRead int64) ([]byte, int) {
+	b1 := make([]byte, bytesToRead)
+	n1, err := file.Read(b1)
+	check(err)
+	return b1, n1
+}
+
+func displayFileInfo(f *os.File) int64 {
+	fileInformation, err := f.Stat()
+	check(err)
+	fmt.Printf("File size: %d bytes\n", fileInformation.Size())
+	return fileInformation.Size()
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+/**
++--------------+----------------+
+|     0xC1     |     0x00       +--> Header 2 bytes
++--------------+----------------+
+|    4 BYTES CRC32 + 16 BYTES   +--> CRC32(Content) + Padding
++-------------------------------+
+|            Content            |
+|  +-------------------------+  |
+|  |         2 BYTES         +-----> Metadata Length
+|  +-------------------------+  |
+|  +-------------------------+  |
+|  |                         |  |
+|  |        Metadata         +-----> Optional Metadata (up to 65535 bytes)
+|  |                         |  |
+|  +-------------------------+  |
+|  +-------------------------+  |
+|  |                         |  |
+|  |       Content Data      +-----> User Data
+|  |                         |  |
+|  +-------------------------+  |
++-------------------------------+
+
+*/
