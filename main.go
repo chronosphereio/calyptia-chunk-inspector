3f1dc1d3de6d637a4da5f56a6f1012c4be1bf704
lecaros <lecaros@calyptia.com>
2022-02-10 19:42:25 -0300

    create structure for subcommands dump and check

--- main.go
+++ main.go
@@ -25,9 +25,41 @@ var verbose *bool
 
 func main() {
 
-	fileName := flag.String("file", "", "File to be processed")
-	verbose = flag.Bool("v", false, "Activates verbose mode")
-	directory := flag.String("dir", ".", "Directory containing the file(s) to process")
+	if len(os.Args) < 2 {
+		fmt.Println("expected subcommand. Exiting")
+		os.Exit(1)
+	}
+	dumpCmd := flag.NewFlagSet("dump", flag.ExitOnError)
+	dumpFlbFile := dumpCmd.String("file", "", "Flb file to be dumped.")
+	dumpOutFile := dumpCmd.String("out", "out.json", "Output file. By default out.json")
+
+	checkCmd := flag.NewFlagSet("check", flag.ExitOnError)
+	fileName := checkCmd.String("file", "", "File to be processed")
+	verbose := checkCmd.Bool("v", false, "Activates verbose mode")
+	directory := checkCmd.String("dir", ".", "Directory containing the file(s) to process")
+
+	switch os.Args[1] {
+	case "dump":
+		err := dumpCmd.Parse(os.Args[2:])
+		check(err)
+		fmt.Println("subcommand 'dump'")
+		fmt.Println("flbFile:", *dumpFlbFile)
+		fmt.Println("dumpOutFile:", *dumpOutFile)
+
+		os.Exit(0)
+	case "check":
+		err := checkCmd.Parse(os.Args[2:])
+		check(err)
+		fmt.Println("Subcommand 'check'")
+		fmt.Println("Filename", *fileName)
+		fmt.Println("verbose", *verbose)
+		fmt.Println("directory", *directory)
+		os.Exit(0)
+	default:
+		fmt.Println("Command expected. Quitting")
+		os.Exit(1)
+	}
+
 	flag.Parse()
 
 	if *verbose {
