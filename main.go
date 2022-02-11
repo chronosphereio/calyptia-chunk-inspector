a5009f1d44e3a9f3802ea77550567e83c2c61051
lecaros <lecaros@calyptia.com>
2022-02-10 21:36:16 -0300

    separate check command from main file

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"encoding/binary"
 	"flag"
 	"fmt"
-	"io/fs"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
-)
-
-const (
-	HeaderBytesQuantity         = 2
-	CRCBytesQuantity            = 4
-	CRCPaddingBytesQuantity     = 16
-	MetadataLengthBytesQuantity = 2
-	FileMetaBytesQuantity       = HeaderBytesQuantity + CRCBytesQuantity + CRCPaddingBytesQuantity + MetadataLengthBytesQuantity
-	MinRequiredFileLength       = FileMetaBytesQuantity + 1
 )
 
 var verbose *bool
@@ -45,15 +31,13 @@ func main() {
 		fmt.Println("subcommand 'dump'")
 		fmt.Println("flbFile:", *dumpFlbFile)
 		fmt.Println("dumpOutFile:", *dumpOutFile)
-
 		os.Exit(0)
 	case "check":
 		err := checkCmd.Parse(os.Args[2:])
 		check(err)
-		fmt.Println("Subcommand 'check'")
-		fmt.Println("Filename", *fileName)
-		fmt.Println("verbose", *verbose)
-		fmt.Println("directory", *directory)
+		options := CheckOption{*fileName, *directory, *verbose}
+		err = Check(options)
+		check(err)
 		os.Exit(0)
 	default:
 		fmt.Println("Command expected. Quitting")
@@ -68,57 +52,6 @@ func main() {
 		log.SetOutput(os.Stderr)
 	}
 
-	if *fileName != "" {
-		checkFile(*fileName)
-		os.Exit(0)
-	}
-
-	if *directory != "" {
-		_, err := ioutil.ReadDir(*directory)
-		check(err)
-		filepath.Walk(*directory,
-			func(path string, info fs.FileInfo, err error) error {
-				if strings.HasSuffix(path, ".flb") {
-					checkFile(path)
-				}
-				return nil
-			})
-	}
-}
-
-func checkFile(fileName string) {
-	fmt.Printf("Filename %s\n", fileName)
-
-	f, err := os.Open(fileName)
-
-	check(err)
-	defer f.Close()
-
-	fileSize := displayFileInfo(f)
-
-	if fileSize < MinRequiredFileLength {
-		fmt.Println("File seems corrupted. Aborting")
-		os.Exit(1)
-	}
-
-	readHeader(f)
-
-	readCRC(f)
-
-	readPadding(f)
-
-	mLength := getMetadataLength(f)
-
-	if mLength > 0 {
-		readMetadata(f, mLength)
-	}
-
-	readUserData(f, mLength, fileSize)
-
-	err = f.Close()
-	check(err)
-
-	fmt.Println("OK")
 }
 
 func readUserData(f *os.File, metadataLength uint16, fileSize int64) {
@@ -133,59 +66,6 @@ func readMetadata(f *os.File, mLength uint16) {
 	bytesRead, content := readNBytesFromFile(f, int64(mLength))
 	if *verbose {
 		fmt.Printf("%d bytes read from Metadata: %s\n", content, string(bytesRead[:content]))
-	}
-}
-
-func getMetadataLength(f *os.File) uint16 {
-	bytesRead, _ := readNBytesFromFile(f, MetadataLengthBytesQuantity)
-	data := binary.BigEndian.Uint16(bytesRead)
-	if *verbose {
-		fmt.Printf("Metadata Length: %d\n", data)
-	}
-	return data
-}
-
-func readPadding(f *os.File) {
-	bytesRead, content := readNBytesFromFile(f, CRCPaddingBytesQuantity)
-	if *verbose {
-		fmt.Printf("%d bytes read from Padding: %s\n", content, string(bytesRead[:content]))
-	}
-}
-
-func readCRC(f *os.File) {
-	bytesRead, content := readNBytesFromFile(f, CRCBytesQuantity)
-	if *verbose {
-		fmt.Printf("%d bytes from CRC: %s\n", content, string(bytesRead[:content]))
-	}
-}
-
-func readHeader(f *os.File) ([]byte, int) {
-	bytesRead, content := readNBytesFromFile(f, HeaderBytesQuantity)
-	if *verbose {
-		fmt.Printf("%d bytes from header: %s\n", content, string(bytesRead[:content]))
-	}
-	return bytesRead, content
-}
-
-func readNBytesFromFile(file *os.File, bytesToRead int64) ([]byte, int) {
-	b1 := make([]byte, bytesToRead)
-	n1, err := file.Read(b1)
-	check(err)
-	return b1, n1
-}
-
-func displayFileInfo(f *os.File) int64 {
-	fileInformation, err := f.Stat()
-	check(err)
-	if *verbose {
-		fmt.Printf("File size: %d bytes\n", fileInformation.Size())
-	}
-	return fileInformation.Size()
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
 	}
 }
 
