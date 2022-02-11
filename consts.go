1efdcd3af293a81209eecccca9c5a85565c54d8d
lecaros <lecaros@calyptia.com>
2022-02-10 21:24:21 -0300

    separate consts

---
+++ consts.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	HeaderBytesQuantity         = 2
+	CRCBytesQuantity            = 4
+	CRCPaddingBytesQuantity     = 16
+	MetadataLengthBytesQuantity = 2
+	FileMetaBytesQuantity       = HeaderBytesQuantity + CRCBytesQuantity + CRCPaddingBytesQuantity + MetadataLengthBytesQuantity
+	MinRequiredFileLength       = FileMetaBytesQuantity + 1
+)
